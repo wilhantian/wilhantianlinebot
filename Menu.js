30ca7e6c327977cf77c2c8253cfe7ffb416f86d8
wilhantian <>
2019-05-31 10:49:36 +0800

    想

--- Menu.js
+++ Menu.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = class Menu{
     constructor(client){
         this.client = client;
@@ -10,6 +12,66 @@ module.exports = class Menu{
             console.log("获取菜单列表:", res)
         }, (err)=>{
             console.error("获取菜单列表异常", err)
-        }).catch((err)=>{console.error("获取菜单列表错误", err)})
+        }).catch((err)=>{console.error("获取菜单列表错误", err)});
+
+        // this.client.createRichMenu({
+        //     size: {
+        //         width: 2500,
+        //         height: 843
+        //     },
+        //     selected: true,
+        //     name: "手柄菜单",
+        //     chatBarText: "手柄",
+        //     areas: [
+        //         {
+        //             bounds: [
+        //                 {
+        //                     x: 327,
+        //                     y: 33,
+        //                     width: 772,
+        //                     height: 772
+        //                 },
+        //                 {
+        //                     x: 1630,
+        //                     y: 303,
+        //                     width: 631,
+        //                     height: 273
+        //                 },
+        //                 {
+        //                     x: 1098,
+        //                     y: 666,
+        //                     width: 534,
+        //                     height: 138
+        //                 }
+        //             ],
+        //             action: [
+        //                 {
+        //                     type: "message",
+        //                     label: "方向键",
+        //                     text: "方向键"
+        //                 },
+        //                 {
+        //                     type: "message",
+        //                     label: "功能键",
+        //                     text: "功能键"
+        //                 },
+        //                 {
+        //                     type: "message",
+        //                     label: "切换键盘",
+        //                     text: "切换键盘"
+        //                 }
+        //             ]
+        //         }
+        //     ]
+        // }).then((id)=>{
+        //     console.log("创建菜单完成", id);
+        //     this.client.setRichMenuImage(id, fs.readFileSync("./img/gamepad.png"), "image/png");
+        // }, (err)=>{
+        //     console.warn("创建菜单失败", err);
+        // }).catch((err)=>{
+        //     console.error("创建菜单错误", err);
+        // });
+
+        console.log(fs.readFileSync("./img/gamepad.png"));
     }
 }
