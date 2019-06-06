ea40a5126238f46fb2021673119c53419a4ab804
wilhantian <>
2019-06-06 10:39:15 +0800

    想

--- index.js
+++ index.js
@@ -249,6 +249,37 @@ regReply("message", function (event) {
     return null;
 });
 
+regReply("message", function(event){
+  if(event.message.type == "text" && event.message.text == "test"){
+    const echo = createWelcomeMsg("-", [
+      {
+        title: "欢迎关注6699.JP",
+        img: "https://static.6699.jp/mp/image/1559188833011_T1QFXxYtVb9eoFzx.png",
+        label: "欢迎关注6699.JP", 
+        url: "https://baidu.com",
+      },
+      {
+        title: "链接1",
+        img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
+        label: "链接1", 
+        url: "https://baidu.com",
+      },
+      {
+        title: "链接2",
+        img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
+        label: "链接2", 
+        url: "https://baidu.com",
+      },
+      {
+        title: "链接3",
+        img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
+        label: "链接3", 
+        url: "https://baidu.com",
+      },
+    ]);
+    return client.replyMessage(event.replyToken, echo);
+  }
+});
 
 
 
@@ -442,4 +473,73 @@ function createGameListMsg(title, img, url){
             }
           }
     };
+}
+
+// @link [{label, url, img, title}]
+function createWelcomeMsg(altText, links){
+  if(!links || links.length == 0){
+    return {};
+  }
+
+  var gameContents = [];
+  for(var i=1; i<links.length; i++){
+    var l = links[i];
+    gameContents.push({
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "md",
+        "action": {
+          "type": "uri",
+          "label": l.label,
+          "uri": l.url
+        },
+        "contents": [
+          {
+                "flex": 3,
+                "type": "text",
+                "text": l.title,
+                "size": "sm",
+                "color": "#000000",
+                "gravity": "center"
+          },
+          {
+            "flex": 1,
+            "type": "image",
+            "url": l.img,
+            "aspectRatio": "1:1"
+          }
+        ]
+    });
+
+    if(i != links.length - 1){
+      gameContents.push({
+        "type": "separator"
+      });
+    }
+  }
+
+  return {
+    type: "flex",
+    altText: altText,
+    contents: {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": links[0].img,
+        "size": "full",
+        "aspectRatio": "1.9:1",
+        "action": {
+          "type": "uri",
+          "label": links[0].label,
+          "uri": links[0].url,
+        }
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "md",
+        "contents": gameContents
+      }
+    }
+  }
 }
