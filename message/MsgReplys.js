bd85e59ff932d9ba261bf58f376dcf23f36cde6e
wilhantian <>
2019-06-10 09:15:22 +0800

    x

--- message/MsgReplys.js
+++ message/MsgReplys.js
@@ -6,28 +6,28 @@ function init() {
     MsgMgr.registerFollow(function (token, userId) {
         const echo = MsgBuilder.createFollowMsg("-", [
             {
-                title: "欢迎关注6699.JP",
-                img: "https://static.6699.jp/mp/image/1559188833011_T1QFXxYtVb9eoFzx.png",
-                label: "欢迎关注6699.JP",
-                url: "https://baidu.com",
+                title: "萌犬",
+                img: "https://box1.fanyoy.com/games/line-demo/res/dog_banner.png",
+                label: "萌犬",
+                url: "https://6699.jp/test/?appid=dog",
             },
             {
-                title: "链接1",
-                img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
-                label: "链接1",
-                url: "https://baidu.com",
+                title: "神经猫",
+                img: "https://box1.fanyoy.com/games/line-demo/res/cat_icon.png",
+                label: "神经猫",
+                url: "line://app/1579130869-NEAZvJoV",
             },
             {
-                title: "链接2",
-                img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
-                label: "链接2",
-                url: "https://baidu.com",
+                title: "射苹果",
+                img: "https://box1.fanyoy.com/games/line-demo/res/apple_icon.png",
+                label: "射苹果",
+                url: "line://app/1579130869-GYBx2Wg0",
             },
             {
-                title: "链接3",
-                img: "https://static.6699.jp/mp/image/1558072291775_WoD6n761QNSoMNKn.png",
-                label: "链接3",
-                url: "https://baidu.com",
+                title: "怪兽",
+                img: "https://box1.fanyoy.com/games/line-demo/res/monster_icon.png",
+                label: "怪兽",
+                url: "line://app/1579130869-1egl9v04",
             },
         ]);
         Line.replyMessage(token, echo);
