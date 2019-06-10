2befcf123b82e6f0b20359f9f1ad4c6e9a471241
wilhantian <>
2019-06-10 16:55:22 +0800

    x

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Menu = require("./Menu");
 const client = require("./Line");
 const MsgMgr = require("./message/MsgMgr");
 const MsgReplys = require("./message/MsgReplys");
+var http = require('http');
+var qs = require('querystring');
 
 const app = express();
 
@@ -32,6 +34,47 @@ app.all('/callback', line.middleware(config.LineConfig), (req, res) => {
 				error: "错误了",
 			})
 		});
+});
+
+app.post('/auth', (request, response) => {
+	var code = request.body.code;
+	var redirect_uri = request.body.redirect_uri;
+	var post_data = {
+		grant_type: 'authorization_code',
+		client_id: '1584819109',
+		client_secret: '1275ffd0b63cc37a85d6498535ca7c36',
+		code: code,
+		redirect_uri: redirect_uri
+	};
+
+	var content = qs.stringify(post_data);
+
+	var options = {
+		hostname: 'https://api.line.me',
+		path: '/v2/oauth/accessToken',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+		}
+	};
+
+	var req = http.request(options, function (res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+			response.json(JSON.parse(chunk));
+		});
+	});
+
+	req.on('error', function (e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	// 将数据写入请求体
+	req.write(content);//注意这个地方  
+	req.end();
 });
 
 // event handler
