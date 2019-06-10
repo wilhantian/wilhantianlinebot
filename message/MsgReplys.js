0fc67c811ea2b3a370fc39974c1fc5a2ee7e8771
wilhantian <>
2019-06-10 11:22:00 +0800

    x

--- message/MsgReplys.js
+++ message/MsgReplys.js
@@ -60,9 +60,7 @@ function init() {
     MsgMgr.registerMsgReply("客服", function (token, msg, userId, time) {
         Line.replyMessage(token, {
             type: "text",
-            text: `如果您有任何问题请通过下方邮箱进行联系我们
-            support@6699.jp
-            我们会在当天进行回复`
+            text: `如果您有任何问题请通过下方邮箱进行联系我们\nsupport@6699.jp\n我们会在当天进行回复`
         });
     });
 }
