bd78d952da01e0a63e9213891d6743d878667479
wilhantian <>
2019-06-10 16:13:53 +0800

    x

--- index.js
+++ index.js
@@ -13,33 +13,35 @@ const app = express();
 Menu.init();
 MsgReplys.init();
 
+app.use(express.static('public'))
+
 app.all('/', (req, res) => {
-    res.send(JSON.stringify({
-        name: 'ok'
-    }))
+	res.send(JSON.stringify({
+		name: 'ok'
+	}))
 });
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.all('/callback', line.middleware(config.LineConfig), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch((err) => {
-            res.json({
-                error: "错误了",
-            })
-        });
+	Promise
+		.all(req.body.events.map(handleEvent))
+		.then((result) => res.json(result))
+		.catch((err) => {
+			res.json({
+				error: "错误了",
+			})
+		});
 });
 
 // event handler
 function handleEvent(event) {
-    console.log("收到消息:", event, event.source.userId);
-    MsgMgr.handle(event);
+	console.log("收到消息:", event, event.source.userId);
+	MsgMgr.handle(event);
 }
 
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+	console.log(`listening on ${port}`);
 });
