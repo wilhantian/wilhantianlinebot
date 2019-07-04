637ad09f17c29985afe612c5ea06df7ad520625e
wilhantian <>
2019-07-04 14:08:57 +0800

    抽屉init

--- src/message/message/reply.js
+++ src/message/message/reply.js
@@ -1,3 +1,5 @@
+const BubbleGames = require('../template/BubbleGames');
+
 module.exports = [
     {
         type: "postback",
@@ -5,10 +7,27 @@ module.exports = [
             "postback.data": "推荐",
         },
         handler: function(event, mgr){
-            return mgr.client.replyMessage(event.replyToken, {
-                type: 'text', 
-                text: '推荐你妹妹'
-            });
+            var message = BubbleGames.create([
+                {
+                    title: 'あなたはワンコがお好き？？',//萌犬
+                    img: 'https://box1.fanyoy.com/games/dog/resource/share_fb/share_1.png',
+                    url: 'line://app/1579130869-Dpk1R65p',
+                    desc: '挂机类',
+                },
+                {
+                    title: '足球',//足球
+                    img: 'https://box1.fanyoy.com/games/dog/resource/share_fb/share_2.png',
+                    url: 'line://app/1579130869-pAZDoX4A',
+                    desc: '动作类',
+                },
+                {
+                    title: 'あなたはワンコがお好き？？',//萌犬
+                    img: 'https://box1.fanyoy.com/games/dog/resource/share_fb/share_3.png',
+                    url: 'line://app/1579130869-Dpk1R65p',
+                    desc: '挂机类',
+                }
+            ])
+            return mgr.client.replyMessage(event.replyToken, message);
         }
     },
     {
