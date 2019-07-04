24efc4f50a1af03260a5b4572d6e387a3f34a15f
wilhantian <>
2019-07-04 13:57:47 +0800

    抽屉init

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const confPub = require('./config-pub');
 const isDebug = true;
 
 const config = isDebug ? confDev : confPub;
-modeule.exports = {
+module.exports = {
     isDebug: isDebug,
     ...config,
     getPublicPath: function(path){
