751e2b8f51fb5d655ff6a4f8b3873aa2087654ae
wilhantian <>
2019-07-04 15:38:52 +0800

    抽屉init

--- src/config/config-dev.js
+++ src/config/config-dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 8080,
+    port: process.env.PORT || 4200,
     channelId: '1595295008',
     channelAccessToken: 'aE8HetKbHR6fr8Ge7qj4WPxZXvg0UVT3l9tg9KAjpbzhevlw9W9QK/uJ2x3j8Y9BAd+SP9RNXoEtza6bS+o3iZlfokYBIuLRWxxlyWxYU5uSSODSsQbnATZdY5RDAX82tVf130Z60zRZX6mTdAeSCAdB04t89/1O/w1cDnyilFU=',
     channelSecret: '52bffc8c35b0e657f3cf26f763d3f411',
