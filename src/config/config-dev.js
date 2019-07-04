ca8ca5f3e76156a9b1c38dd418955a62fc14661e
wilhantian <>
2019-07-04 16:08:50 +0800

    抽屉init

--- src/config/config-dev.js
+++ src/config/config-dev.js
@@ -5,5 +5,5 @@ module.exports = {
     channelAccessToken: 'aE8HetKbHR6fr8Ge7qj4WPxZXvg0UVT3l9tg9KAjpbzhevlw9W9QK/uJ2x3j8Y9BAd+SP9RNXoEtza6bS+o3iZlfokYBIuLRWxxlyWxYU5uSSODSsQbnATZdY5RDAX82tVf130Z60zRZX6mTdAeSCAdB04t89/1O/w1cDnyilFU=',
     channelSecret: '52bffc8c35b0e657f3cf26f763d3f411',
     messageCallbackURI: '/callback',
-    cdnURL: 'https://wilhantianlinebot.herokuapp.com/cdn',
+    cdnURL: 'https://test.6699.jp/msgapi/cdn',
 }
